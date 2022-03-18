b981a49ebf14e35fc3246cba1f1774e46e9e57bd
henri <henriqueyeguo.yeguo@gmail.com>
2022-03-18 10:30:46 +0100

    Prueba 1 zanahoria

--- prm/src/prueba3/prueba.java
+++ prm/src/prueba3/prueba.java
@@ -2,4 +2,17 @@ package prueba3;
 
 public class prueba {
 
+	private String zanahoria;
+
+	public String getZanahoria() {
+		return zanahoria;
+	}
+
+	public void setZanahoria(String zanahoria) {
+		this.zanahoria = zanahoria;
+	}
+	
+	
+	
+	
 }
