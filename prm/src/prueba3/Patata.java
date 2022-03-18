633df53577702793841f750a214f6ac1c3e4886a
henri <henriqueyeguo.yeguo@gmail.com>
2022-03-18 10:01:21 +0100

    Getters and setters patata

--- prm/src/prueba3/Patata.java
+++ prm/src/prueba3/Patata.java
@@ -2,6 +2,14 @@ package prueba3;
 
 public class Patata {
 	private String patata;
+
+	public String getPatata() {
+		return patata;
+	}
+
+	public void setPatata(String patata) {
+		this.patata = patata;
+	}
 	
 	
 }
